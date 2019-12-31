293b5b26b5aaad3bf0482d17a043507c87bf0bba
Robert Horlings <robert@isdat.nl>
2019-12-31 11:31:49 +0100

    Only apply large cache times for statistics

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,13 @@ Route::post('/transactions/stats', 'TransactionController@generateStats');
 Route::post('/accounts/stats', 'AccountController@generateStats');
 Route::post('/categories/stats', 'CategoryController@generateStats');
 
+Route::apiResource('categories', 'CategoryController');
+Route::apiResource('accounts', 'AccountController');
+Route::apiResource('transactions', 'TransactionController');
+Route::get('/categories/tree', 'CategoryController@tree');
+
 Route::middleware('cache.headers:private;max_age=3600')->group(function() {
-    Route::get('/categories/tree', 'CategoryController@tree');
     Route::get('/accounts/stats', 'AccountController@stats');
     Route::get('/categories/stats', 'CategoryController@stats');
     Route::get('/categories/{category}/stats', 'CategoryController@categoryStats');
-    Route::apiResource('categories', 'CategoryController');
-    Route::apiResource('accounts', 'AccountController');
-    Route::apiResource('transactions', 'TransactionController');
 });

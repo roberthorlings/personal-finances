f7ec03b17a8662f204cdba93c1229190513e8422
Robert Horlings <robert@isdat.nl>
2020-01-03 12:44:07 +0100

    Shuffle routes to prevent caching on stats

--- routes/api.php
+++ routes/api.php
@@ -17,18 +17,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/transactions/import', 'TransactionController@import');
-Route::post('/transactions/stats', 'TransactionController@generateStats');
-Route::post('/accounts/stats', 'AccountController@generateStats');
-Route::post('/categories/stats', 'CategoryController@generateStats');
-
-Route::apiResource('categories', 'CategoryController');
-Route::apiResource('accounts', 'AccountController');
-Route::apiResource('transactions', 'TransactionController');
-Route::get('/categories/tree', 'CategoryController@tree');
-
 Route::middleware('cache.headers:private;max_age=3600')->group(function() {
     Route::get('/accounts/stats', 'AccountController@stats');
     Route::get('/categories/stats', 'CategoryController@stats');
     Route::get('/categories/{category}/stats', 'CategoryController@categoryStats');
 });
+
+Route::post('/transactions/import', 'TransactionController@import');
+Route::post('/transactions/stats', 'TransactionController@generateStats');
+Route::post('/accounts/stats', 'AccountController@generateStats');
+Route::post('/categories/stats', 'CategoryController@generateStats');
+
+Route::get('/categories/tree', 'CategoryController@tree');
+Route::apiResource('categories', 'CategoryController');
+Route::apiResource('accounts', 'AccountController');
+Route::apiResource('transactions', 'TransactionController');
